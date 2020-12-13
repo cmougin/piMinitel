5bfe90827c943f3095bd2390e687d831db0d2752
Christophe Mougin <christophe.mougin@laposte.net>
2020-12-13 21:31:14 +0100

    a few traces

--- afterbootstrap.sh
+++ afterbootstrap.sh
@@ -1,3 +1,4 @@
 ## lancé après le bootstrap du Minitel
 ## pour l'instant version pauvre: appel du navigateur Lynx
+echo "debut de $0"
 /usr/bin/lynx www.google.fr
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 ## lancé au démarrage par .bashrc quand on se loggue en tant que "pi" 
 ## et que le terminal est un minitel (commence par m1 ou m2)
+echo "debut de $0"
 cd /home/pi/src/Minitel
 git pull ## remet à jour le dossier à partir du repo GITHUB
-source afterbootstrap.sh ## et continue avec une version éventuellement mise à jour
+echo "appel de afterboostrap.sh"
+afterbootstrap.sh ## et continue avec une version éventuellement mise à jour
