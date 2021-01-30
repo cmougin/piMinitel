2e6ff5a43b1107393d5e20b3ddf8238ee48c6af9
Christophe Mougin <christophe.mougin@laposte.net>
2021-01-30 16:53:18 +0100

    ajout du 2e git pull

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,4 @@ cd /home/pi/Minitel
 git pull ## remet à jour le dossier à partir du repo GITHUB
 echo "appel de afterboostrap.sh"
 afterbootstrap.sh ## et continue avec une version éventuellement mise à jour
+git pull #deuxième chance. parfois le 1er ne passe pas car le réseau n"est pas encore monté. de plus c'est interessant si on a corrigé un bug entre deux
